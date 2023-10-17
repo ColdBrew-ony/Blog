86518bf7977681add51f1a1e72d457cb128fd296
woonny <kjw8739@gmail.com>
2023-10-17 12:53:51 +0900

    2023-10-17 kjw8739 Title : 불필요 삭제 및 레이아웃 테스트 Contents  - 기본 파일 삭제(css,js)  - 레이아웃 테스트

--- src/index.js
+++ src/index.js
@@ -7,12 +7,11 @@ import LayoutMain from './layout/LayoutMain';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
     <Header />
     <LayoutMain />
     <App />
     <Footer />
-  </React.StrictMode>
+  </>
 );
 
-reportWebVitals();
