21effefa5d0d39c512379e8b18fe42bab6523fc0
woonny <kjw8739@gmail.com>
2023-10-17 11:17:55 +0900

    2023-10-17 kjw8739 Title : 불필요 삭제 및 레이아웃 테스트 Contents  - 기본 파일 삭제(css,js)  - 레이아웃 테스트

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import LayoutMain from './layout/LayoutMain';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
+    <LayoutMain />
     <App />
+    <Footer />
   </React.StrictMode>
 );
 
---
+++ src/layout/LayoutMain.js
@@ -0,0 +1,13 @@
+/* LayoutMain.js */
+import React from 'react';
+
+const LayoutMain = () => {
+  return (
+    <header>
+      레이아웃 메인입니다.
+      <hr/>
+    </header>
+  );
+};
+
+export default LayoutMain;
